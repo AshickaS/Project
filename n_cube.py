12936f6d44308cee0baeeaed54e8c0e841ede7e4
AshickaS <112202012@smail.iitpkd.ac.in>
2023-08-28 16:37:22 +0530

    Update n_cube.py

--- n_cube.py
+++ n_cube.py
@@ -3,16 +3,16 @@ import itertools
 import matplotlib.pyplot as plt
 
 def n_cube(n):
-	group = list(itertools.product(range(2), repeat=n))
-	generators = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+	group = [''.join(map(str, p)) for p in itertools.product(range(2), repeat = n)]
+	generators = ['0' * i + '1' + '0' * (n - i - 1) for i in range(n)]
 	G = nx.DiGraph()
 	G.add_nodes_from(group)
 	for generator in generators:	
 		for tail in G.nodes:
-			head = [0]*n
+			head = ''
 			for i in range(n):
-				head[i] = (tail[i] + generator[i])%2
-			G.add_edge(tuple(tail), tuple(head))
+				head += str((int(tail[i]) + int(generator[i])) % 2)		
+			G.add_edge(tail, head)
 		flag = True
 		for u,v in G.edges:
 			if not G.has_edge(v,u):
@@ -20,6 +20,10 @@ def n_cube(n):
 				break
 		if flag:
 			G = G.to_undirected()
-	nx.draw(G, with_labels = True)
-	plt.show()
+	A = nx.adjacency_matrix(G)
+	B = A.todense()
+	C = '\n'.join([' '.join([str(x) for x in row]) for row in B])
+	f = open('matrix.txt', 'w')
+	print(C, file = f)		
+
 n_cube(3)		
