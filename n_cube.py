93d21eeebafbb318dcb78dcf8a824a464f314093
AshickaS <112202012@smail.iitpkd.ac.in>
2023-09-03 15:17:14 +0530

    Update n_cube.py
    Check the graph and make it undirected using functions, calculate the execution time

--- n_cube.py
+++ n_cube.py
@@ -1,8 +1,11 @@
 import networkx as nx
 import itertools
 import matplotlib.pyplot as plt
+import time
+import functions
 
-def n_cube(n):
+def n_cube1(n):
+	initial_time = time.time()
 	group = [''.join(map(str, p)) for p in itertools.product(range(2), repeat = n)]
 	generators = ['0' * i + '1' + '0' * (n - i - 1) for i in range(n)]
 	G = nx.DiGraph()
@@ -13,17 +16,14 @@ def n_cube(n):
 			for i in range(n):
 				head += str((int(tail[i]) + int(generator[i])) % 2)		
 			G.add_edge(tail, head)
-		flag = True
-		for u,v in G.edges:
-			if not G.has_edge(v,u):
-				flag = False
-				break
-		if flag:
-			G = G.to_undirected()
+	G = functions.to_undirected(G)	
 	A = nx.adjacency_matrix(G)
 	B = A.todense()
 	C = '\n'.join([' '.join([str(x) for x in row]) for row in B])
 	f = open('matrix.txt', 'w')
-	print(C, file = f)		
+	print(C, file = f)	
+	final_time = time.time()
+	time_taken  = final_time - initial_time
+	print('Time1 =', time_taken)	
 
-n_cube(3)		
+n_cube1(3)		
