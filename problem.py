18200903d19ea81e7045daff94a46ec67a49408d
AshickaS <112202012@smail.iitpkd.ac.in>
2023-09-08 06:34:13 +0530

    Update problem.py
    Generate a random sample of bit vectors using random integer.
    Add new parameters n1 which represents the value of n in n-cube, n2 which represents the function number of the two functions n_cube1 or n_cube2.

--- problem.py
+++ problem.py
@@ -1,23 +1,26 @@
 import networkx as nx 
 import numpy as np
-import itertools
 from tabulate import tabulate
 import random
 
-def minimum_diameter(input_file, sample_size): 
+def minimum_diameter(input_file, sample_size, n1, n2): 
     input_graph = np.loadtxt(input_file)      #load the input file
     G = nx.Graph(input_graph)         #create a graph
     m = G.number_of_edges()     #size of the graph
     n = G.number_of_nodes() 	#order of the graph
-    f = open('output.txt', 'w')
+    f = open(f'output{n2}_{n1}.txt', 'w')
     F = nx.adjacency_matrix(G)      #create the adjacency matrix
     K = F.toarray()
     print(f'Input graph:\n\n {K}',file = f)         #print the input graph to the file
     print(f'The diameter of the input graph is {nx.diameter(G)}', file = f) 		#print the diameter
     diameters = []      #create a list for diameters
     result = []
-    bit_vector_list = list(itertools.product(range(2), repeat=m))         #create a list of bit vectors
-    bit_vector_sample = random.sample(bit_vector_list, sample_size)         #create a random sample of bit vectors    
+    bit_vector_sample = []      #create a sample of bit vectors
+    for i in range (sample_size):
+        v = [0] * m
+        for j in range (len(v)):
+            v[j] = random.randint(0,1)
+        bit_vector_sample.append(tuple(v))
     for vector in bit_vector_sample:
         DG = nx.DiGraph() 		#create a digraph for each vector
         DG.add_nodes_from([i for i in range (n)])
@@ -41,4 +44,3 @@ def minimum_diameter(input_file, sample_size):
     else:
         print(f'\nMinimum among the diameters is {min(diameters)}', file = f)
 
-minimum_diameter('input3.txt', 100)
