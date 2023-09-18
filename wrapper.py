ec53ba6445a2695528985e658bee86ddbab42456
AshickaS <112202012@smail.iitpkd.ac.in>
2023-09-18 10:39:52 +0530

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -3,7 +3,7 @@ import n_cube
 
 for i in range (3, 11):
 	print('n =', i)
-	n_cube.n_cube1(i)
-	problem.minimum_diameter(f'matrix1_n{i}.txt', 1000, i, 1)
-	n_cube.n_cube2(i)
-	problem.minimum_diameter(f'matrix2_n{i}.txt', 1000, i, 2)
+	G1 = n_cube.n_cube1(i)
+	problem.minimum_diameter(G1, 1000, i, 1)
+	G2 = n_cube.n_cube2(i)
+	problem.minimum_diameter(G2, 1000, i, 2)
