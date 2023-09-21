5aaea143a6dc475baf1fe3cca2b0925a5a611dbf
AshickaS <112202012@smail.iitpkd.ac.in>
2023-09-21 23:37:18 +0530

    Update eulerian.py
    Add the function minimum_diameter. Run the function eulerian_circuit sample size many times and find the minimum diameter.

--- eulerian.py
+++ eulerian.py
@@ -1,22 +1,35 @@
 import networkx as nx
 import random
+from n_cube import n_cube2
 
 def eulerian_circuit(G):
-    D = G.copy()
-    DG = nx.DiGraph() 
+    D = G.copy()        #remove the vertices from this graph
+    DG = nx.DiGraph()       #add the vertices to this graph 
     DG.add_nodes_from(G)
-    V = list(G.nodes())
+    V = list(G.nodes())     #vertex set of G
     while D.number_of_edges() > 0:
-        v = random.choice(V)
+        v = random.choice(V)        #select a random vertex in G
         while True:
             #print(v)
-            N = list(D.neighbors(v))
-            if len(N) == 0:
+            N = list(D.neighbors(v))        #N(v)
+            if len(N) == 0:     #break the loop if N(v) is empty
                 break
-            u = random.choice(N)
-            DG.add_edge(v, u)
-            D.remove_edge(v, u)
-            v = u
-    return DG
+            u = random.choice(N)        #select a random neighbour of v
+            DG.add_edge(v, u)       #add edge to resultant digraph DG
+            D.remove_edge(v, u)     #remove the edge from D
+            v = u       #repeat the process at u
+    return nx.diameter(DG)
+
+def minimum_diameter(G, sample_size):
+    diameters = set()
+    for i in range(sample_size):
+        diameter = eulerian_circuit(G)      #calculate the diameter sample size many timess
+        diameters.add(diameter)     #store the diameters
+    return min(diameters)       #return the minimum diameter
+
+G = n_cube2(4)
+sample_size = 100
+print(minimum_diameter(G, sample_size))
+
  
 
