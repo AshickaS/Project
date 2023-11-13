deffcd36a301635709ba2dc14098cd46a3f1cb63
AshickaS <112202012@smail.iitpkd.ac.in>
2023-11-13 10:31:22 +0530

    Update relabel.py
    Add the function bulk_relabel

--- relabel.py
+++ relabel.py
@@ -17,3 +17,35 @@ def relabel(s1, s2):
 		result += values[i]
 	result += s2
 	return result
+
+def bulk_relabel(s1_list, s2):
+    '''
+    The function bulk_relabel() checks whether all the strings in s1_list have the same characters and return a list of relabelled strings in s1_list. 
+    '''
+    labels = 'abcdefghijklmnopqrstuvwxyz'
+    results = []
+    try:
+        s1_sets = [set(s1) for s1 in s1_list]
+        if not all(s == s1_sets[0] for s in s1_sets):
+            raise Exception('All strings in s1_list must have the same set of characters.')
+		
+        values = dict()
+        labels_list = sorted(s1_list[0])
+        used = set()    
+        for i in labels_list:
+            j = i
+            while j in s2 or j in used:
+        	    j = labels[labels.index(j) + 1]
+            used.add(j)
+            values[i] = j
+            
+        for s1 in s1_list:
+            result = ''
+            for i in s1:
+                result += values[i]
+            result += s2
+            results.append(result)
+    except Exception as e:
+        print(e)
+    return results
+
