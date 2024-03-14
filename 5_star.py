de17290924f2688ec8e28227000013c77ad7b331
AshickaS <112202012@smail.iitpkd.ac.in>
2024-03-14 21:17:25 +0530

    Update 5_star.py
    Shuffle the path lengths dictionary

--- 5_star.py
+++ 5_star.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import random as random
 from eulerian import path
 from star_graph import n_star
 
@@ -6,10 +7,14 @@ G = n_star(5)
 count = 0
 path_lengths = nx.all_pairs_shortest_path_length(G)
 path_lengths_dict = {(u, v): d for u, paths in path_lengths for v, d in paths.items()}
-sorted_pairs = sorted(path_lengths_dict.items(), key=lambda item: item[1], reverse=True)
+path_lengths_list = list(path_lengths_dict.items())
+random.shuffle(path_lengths_list)
+path_lengths_dict_shuffled = dict(path_lengths_list)
+sorted_pairs = sorted(path_lengths_dict_shuffled.items(), key=lambda item: item[1], reverse=True)
 K = nx.DiGraph(G)
 for (u, v), d in sorted_pairs:
     P = path(K, u, v)
+    print(count, nx.diameter(K), len(P)-1, nx.is_eulerian(K))
     count += 1
     # for i in range(len(P) - 1):
     #     if (P[i+1], P[i]) in K.edges():
