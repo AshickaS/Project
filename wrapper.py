870f0c420ba11795b07c5834cfe4ddfd2c130795
AshickaS <112202012@smail.iitpkd.ac.in>
2024-05-12 23:07:02 +0530

    Delete unwanted files

--- wrapper.py
+++
@@ -1,9 +0,0 @@
-import problem
-import n_cube
-
-for i in range (3, 11):
-	print('n =', i)
-	G1 = n_cube.n_cube1(i)
-	problem.minimum_diameter(G1, 1000, i, 1)
-	G2 = n_cube.n_cube2(i)
-	problem.minimum_diameter(G2, 1000, i, 2)
